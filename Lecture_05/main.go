f07fc72b314d06864e129dd5f7906ed550cdbddc
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-06-18 17:31:40 +0600

    lecture 5

---
+++ Lecture_05/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main(){
+
+ fmt.Println("Assalamu Alaikum")
+ fmt.Println("Hello world")
+
+
+//class 8
+
+var name string = "TAREQ"
+var name2 , city string = "Ibna", "DHAKA"
+fmt.Println(name, city, name2)
+
+var c rune = 'A'
+var age int = 10
+var result float32 = 50.95
+
+fmt.Println(c,age,result)
+
+
+//class 9
+
+var name string
+var new_age int
+var result_new float32
+var chr rune
+
+
+fmt.Println(name)
+fmt.Println(new_age)
+fmt.Println(result_new)
+fmt.Println(chr)
+
+
+
+}
+
+
+
+
+
+
