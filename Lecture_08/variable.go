bef26f69b813a9c0fc85e6287f75ec249cfb15c4
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-06-19 21:56:02 +0600

    golang variable

---
+++ Lecture_08/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//var name string = "TAREQ"
+//var name2 , city string = "Ibna", "DHAKA"
+//fmt.Println(name, city, name2)
+
+var c rune = 'A'
+var age int = 10
+var result float32 = 50.95
+
+fmt.Println(c,age,result)
+}
+
+
+
