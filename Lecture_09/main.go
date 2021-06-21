e595c20b90a474bc045e6ee1d8d3df2c576a4553
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-06-21 23:18:01 +0600

    lecture9

---
+++ Lecture_09/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main(){
+
+var isFound bool
+
+
+fmt.Println(isFound)
+fmt.Println(true && true)
+ fmt.Println(true && false)
+ fmt.Println(true || true)
+ fmt.Println(true || false)
+ fmt.Println(!true)
+
+
+//var name string
+//var age int
+//var result float32
+var chr rune
+chr = 'অ'
+
+
+//fmt.Println(name)
+//fmt.Println(age)
+//fmt.Println(result)
+fmt.Println(chr)
+fmt.Printf("%c", chr)
+
+
+}
