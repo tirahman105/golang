aca23e9db33b2b8718132c03ae5a87090483c421
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-06-30 11:44:54 +0600

    lecture 10

---
+++ Lecture_10/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+//map
+
+x := make(map[string]string)
+x["name"] = "Tareq"
+x["height"] = "5.8"
+x["Address"] = "Dhaka"
+
+fmt.Println(x)
+
+}
+
+
+
+
+
+
+
+ 
+
+
+
+
